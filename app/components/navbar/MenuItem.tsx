bb18ef82d90cceb649364637ac59b4ba9d4f764d
Rajpreet0 <rajpreet.singh@gmx.de>
2024-08-19 10:28:44 +0200

    login functionality

--- app/components/navbar/MenuItem.tsx
+++ app/components/navbar/MenuItem.tsx
@@ -3,11 +3,12 @@
 interface MenuItemProps {
     onClick: () => void;
     label: string;
+    isRed?: boolean;
 }
 
-const MenuItem: React.FC<MenuItemProps> = ({onClick, label}) => {
+const MenuItem: React.FC<MenuItemProps> = ({onClick, label, isRed}) => {
   return (
-    <div onClick={onClick} className="px-4 py-3 hover:bg-neutral-100 transition font-semibold">
+    <div onClick={onClick} className={`px-4 py-3 hover:bg-neutral-100 transition font-semibold ${isRed ? 'text-red-500': 'text-black'}`}>
         {label}
     </div>
   )
