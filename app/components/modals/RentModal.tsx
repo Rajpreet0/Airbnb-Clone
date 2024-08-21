2e6ad050e1a0ff3c1197d788289ab709f0fc1760
Rajpreet0 <rajpreet.singh@gmx.de>
2024-08-21 10:34:00 +0200

    added Counter in the Listing Modal

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -9,7 +9,9 @@ import CategoryInput from "../inputs/CategoryInput";
 import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
+import Counter from "../inputs/Counter";
 
+// Steps inside the Modal for Listing
 enum STEPS {
     CATEGORY = 0,
     LOCATION = 1,
@@ -49,7 +51,11 @@ const RentModal = () => {
 
     const category = watch('category');
     const location = watch('location');
+    const guestCount = watch('guestCount');
+    const roomCount = watch('roomCount');
+    const bathroomCount = watch('bathroomCount');
 
+    // Dynamically load the Custom Map, bc it is not supported by Next
     const Map = useMemo(() => dynamic(() => import('../Map'), {
         ssr: false
     }), [location]);
@@ -63,6 +69,8 @@ const RentModal = () => {
         })
     }
 
+    // Controllings in the Modal
+
     const onBack = () => {
         setStep((value) => value - 1);
     };
@@ -72,6 +80,7 @@ const RentModal = () => {
     }
 
 
+    // Set the Button Text based on the Step
     const actionLabel = useMemo(() => {
         if (step === STEPS.PRICE) {
             return 'Create';
@@ -80,6 +89,7 @@ const RentModal = () => {
         return 'Next';
     }, [step]);
 
+    // Set the secondary Button Text based on the Step
     const secondaryActionLabel = useMemo(() => {
         if (step === STEPS.CATEGORY) {
             return undefined
@@ -88,6 +98,7 @@ const RentModal = () => {
         return 'Back';
     }, [step])
 
+    // Set the HTML Content on the Modal
     let bodyContent = (
         <div className="flex flex-col gap-8">
             <Heading 
@@ -95,6 +106,7 @@ const RentModal = () => {
                 subtitle="Pick a category"
             />
             <div className="grid gird-cols-1 md:grid-cols-2 gap-3 max-h-[50vh] overflow-y-auto">
+                {/* Load the categories Object Array from NavBar */}
                 {categories.map((item) => (
                     <div key={item.label} className="col-span-1">
                         <CategoryInput
@@ -109,6 +121,7 @@ const RentModal = () => {
         </div>    
     )
 
+    // Set the HTML Content for the Location Step on the Modal whith Dropdown Menu and the Map
     if (step === STEPS.LOCATION) {
         bodyContent = (
             <div className="flex flex-col gap-8">
@@ -122,6 +135,35 @@ const RentModal = () => {
                 <Map
                     center={location?.latlng}
                 />
+            </div>
+        )
+    }
+
+    // Set the HTML Content for the Counter Step on the Modal
+    if(step === STEPS.INFO) {
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading
+                    title="Share some basics about your place"
+                    subtitle="What amenities do you have ?"
+                />
+                <Counter
+                    title="Guests"
+                    subtitle="How many guests do you allow ?"
+                    value={guestCount}
+                    onChange={(value) => setCustomValue('guestCount', value)}/>
+                <hr/>
+                <Counter
+                    title="Rooms"
+                    subtitle="How many rooms do you have ?"
+                    value={roomCount}
+                    onChange={(value) => setCustomValue('roomCount', value)}/>
+                <hr/>
+                <Counter
+                    title="Bathrooms"
+                    subtitle="How many bathrooms do you have ?"
+                    value={bathroomCount}
+                    onChange={(value) => setCustomValue('bathroomCount', value)}/>
             </div>
         )
     }
